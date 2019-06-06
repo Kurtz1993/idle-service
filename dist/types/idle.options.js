fb1678b2dd060733a54eb45e48a556b612797e1b
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-06 12:53:26 -0500

    Add es5 dist files

--- dist/types/idle.options.js
+++ dist/types/idle.options.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class IdleOptions {
+var IdleOptions = /** @class */ (function () {
     /**
      * Specifies the options for the idle service.
      * @param timeToIdle Inactive time in seconds that the user needs to be considered idle.
@@ -8,12 +8,17 @@ class IdleOptions {
      * @param autoResume Specifies if the service should auto resume itself after the user is considered idle.
      * @param listenFor DOM events to listen for the user to be considered active.
      */
-    constructor(timeToIdle = 20 * 60, timeToTimeout = 30, autoResume = true, listenFor = "keydown mousewheel mousedown touchstart touchmove scroll") {
+    function IdleOptions(timeToIdle, timeToTimeout, autoResume, listenFor) {
+        if (timeToIdle === void 0) { timeToIdle = 20 * 60; }
+        if (timeToTimeout === void 0) { timeToTimeout = 30; }
+        if (autoResume === void 0) { autoResume = true; }
+        if (listenFor === void 0) { listenFor = "keydown mousewheel mousedown touchstart touchmove scroll"; }
         this.timeToIdle = timeToIdle;
         this.timeToTimeout = timeToTimeout;
         this.autoResume = autoResume;
         this.listenFor = listenFor;
     }
-}
+    return IdleOptions;
+}());
 exports.IdleOptions = IdleOptions;
 //# sourceMappingURL=idle.options.js.map
--- dist/types/idle.state.js
+++ dist/types/idle.state.js
@@ -1,17 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class UserState {
+var UserState = /** @class */ (function () {
     /**
      * Represents the current state of the user.
      * @param isIdle Specifies if the user is idle.
      * @param hasTimedout Specifies if the user has timed out.
      * @param userInactivityTime Number of seconds the user has been inactive.
      */
-    constructor(isIdle = false, hasTimedout = false, userInactivityTime = 0) {
+    function UserState(isIdle, hasTimedout, userInactivityTime) {
+        if (isIdle === void 0) { isIdle = false; }
+        if (hasTimedout === void 0) { hasTimedout = false; }
+        if (userInactivityTime === void 0) { userInactivityTime = 0; }
         this.isIdle = isIdle;
         this.hasTimedout = hasTimedout;
         this.userInactivityTime = userInactivityTime;
     }
-}
+    return UserState;
+}());
 exports.UserState = UserState;
 //# sourceMappingURL=idle.state.js.map
